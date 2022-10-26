ed7edea58ae9fff575220db6729e471285328133
yuki_obata <yuki.obata@appirits.com>
2022-10-26 18:57:45 +0900

    26日更新

--- javascript/main.js
+++ javascript/main.js
@@ -51,3 +51,37 @@ $(window).on('load', function () {
 
   EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+/* --------------------------------------------------
+  検索窓動作 クリック時にモーダルで検索フォーム表示
+-------------------------------------------------- */ 
+//開くボタンを押した時には
+$(".search-open-btn").click(function () {
+  $(".search-wrap").addClass('panelactive');//#search-wrapへpanelactiveクラスを付与
+$('#search-text').focus();//テキスト入力のinputにフォーカス
+});
+
+//閉じるボタンを押した時には
+$(".search-close-btn").click(function () {
+  $(".search-wrap").removeClass('panelactive');//#search-wrapからpanelactiveクラスを除去
+});
+
+/* --------------------------------------------------
+  .header-fixed ヘッダー固定表示
+-------------------------------------------------- */ 
+let _window = $(window),
+    _header = $('.is-top-header'),
+    headerChange = $('.is-fixed-header'),
+    heroBottom;
+    
+    _window.on('scroll', function() {
+      heroBottom = $('.is-hero-img').height();
+      if(_window.scrollTop() > heroBottom) {
+        headerChange.addClass('is-show');
+      }
+      else {
+        headerChange.removeClass('is-show');
+      }
+    });
+_window.trigger('scroll');
+
