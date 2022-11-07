9bef7b49240de121c4d4bc9db6b883562d1bd2ce
yuki_obata <yuki.obata@appirits.com>
2022-11-07 17:14:23 +0900

    更新

--- javascript/main.js
+++ javascript/main.js
@@ -52,6 +52,7 @@ $(window).on('load', function () {
   EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
+
 /* --------------------------------------------------
   検索窓動作 クリック時にモーダルで検索フォーム表示
 -------------------------------------------------- */ 
@@ -65,6 +66,7 @@ $('#search-text').focus();//テキスト入力のinputにフォーカス
 $(".search-close-btn").click(function () {
   $(".search-wrap").removeClass('panelactive');//#search-wrapからpanelactiveクラスを除去
 });
+
 
 /* --------------------------------------------------
   .header-fixed ヘッダー固定表示
@@ -85,3 +87,71 @@ let _window = $(window),
     });
 _window.trigger('scroll');
 
+
+/* --------------------------------------------------
+  タブメニューリンク
+-------------------------------------------------- */ 
+function GethashID(hashIDName) {
+  if(hashIDName) {
+    // タブ内のaタグを全て取得
+    $('.tab-nav__item').find('a').each(function() {
+      let idName = $(this).attr('href');
+      // リンク元の指定されたURLのハッシュタグ
+      if(idName == hashIDName) {
+        let parentElm = $(this).parent();
+        $('.tab-nav__item').removeClass("active");
+        $(parentElm).addClass("active");
+        $(".tab-contents").removeClass("is-active");
+        $(hashIDName).addClass("is-active");
+      }
+    });
+  }
+}
+
+// タブをクリックしたら
+$('.tab-nav__link').on('click', function() {
+  let idName = $(this).attr('href');
+  GethashID(idName);
+  return false;
+});
+
+$(window).on('load', function() {
+  $('.tab-nav__item:first-of-type').addClass("active");
+  $('.tab-contents:first-of-type').addClass("is-active");
+  let hashName = location.hash;
+  GethashID(hashName);
+});
+
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+	if(hashIDName){
+		//タブ設定
+		$('.tab__item').find('a').each(function() { //タブ内のaタグ全てを取得
+			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
+			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+				$('.tab__item').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+				//表示させるエリア設定
+				$(".tab__area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+			}
+		});
+	}
+}
+
+//タブをクリックしたら
+$('.tab__link').on('click', function() {
+	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
+	GethashID (idName);//設定したタブの読み込みと
+	return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab__item:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.tab__area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+	GethashID (hashName);//設定したタブの読み込み
+});
