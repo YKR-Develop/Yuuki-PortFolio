38d5c9aef946fe9c1283a16e26bd2db0fb81fd9f
yukuro-portfolio <116317761+yukuro-portfolio@users.noreply.github.com>
2023-01-31 13:20:24 +0900

    更新

--- javascript/main.js
+++ javascript/main.js
@@ -87,6 +87,33 @@ let _window = $(window),
     });
 _window.trigger('scroll');
 
+/* --------------------------------------------------
+  .header-fixed ブログ用ヘッダー固定表示 
+-------------------------------------------------- */ 
+//スクロールすると上部に固定させるための設定を関数でまとめる
+function FixedAnime() {
+  let headerH = $('.header-blog').outerHeight(true);
+  let scroll = $(window).scrollTop();
+  if (scroll >= headerH){//headerの高さ以上になったら
+      $('.header-blog').addClass('fixed');//fixedというクラス名を付与
+    }else{//それ以外は
+      $('.header-blog').removeClass('fixed');//fixedというクラス名を除去
+    }
+}
+
+//ナビゲーションをクリックした際のスムーススクロール
+$('.header-blog__link').click(function () {
+  let elmHash = $(this).attr('href'); //hrefの内容を取得
+  let pos = Math.round($(elmHash).offset().top-120);  //headerの高さを引く
+  $('body,html').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
+  return false;//リンクの無効化
+});
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
 
 /* --------------------------------------------------
   タブメニューリンク
@@ -176,14 +203,9 @@ jQuery(function($) {
 /* --------------------------------------------------
   ナビゲーションメニュー 滞在ページの表示変更 worksページ
 -------------------------------------------------- */ 
-let navLink = document.getElementsByClassName('works-nav__link');
+let navLink = document.getElementsByClassName('category-nav__link');
     for (let i = 0; i < navLink.length; i++) {
       if(navLink[i].href === location.href) {
           navLink[i].classList.add('style-change');
         }
     }
-
-
-/* --------------------------------------------------
-  タブメニュー ページ遷移リンクの指定
--------------------------------------------------- */ 
